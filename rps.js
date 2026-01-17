7f5408adaa70840bfc1e14f119d7b9d7e79e9109
Adrianto Puji Irawan <adrianto.irawan@gmail.com>
2026-01-17 08:54:38 +0700

    Add variables to keep track of player's scores

--- rps.js
+++ rps.js
@@ -1,5 +1,11 @@
 console.log("Hello World");
 
+// INIT humanScore = 0 to keep track of human player's score
+let humanScore = 0;
+
+// INIT computerScore = 0 to keep track of computer player's score
+let computerScore = 0;
+
 function getComputerChoice() {
     // INIT randomVariable with a CALL to getRandomInt(3) to get either 0, 1, 2 randomly
     const randomVariable = getRandomInt(3);
@@ -26,9 +32,14 @@ function getRandomInt(max) {
 
 function getHumanChoice() {
     // INIT humanChoice variable with a CALL to prompt() with a message asking the user what hand they would play
+    let humanChoice = prompt("What hand would u play?");
 
     // Return the humanChoice as is
+    return humanChoice;
 }
 
 console.log("getComputerChoice() result: ");
 console.log(getComputerChoice());
+
+console.log("getHumanChoice() result: ");
+console.log(getHumanChoice());
