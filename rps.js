a27d7a8b7c398628fd07c2a26e099448d97d41ab
Adrianto Puji Irawan <adrianto.irawan@gmail.com>
2026-01-17 08:44:59 +0700

    Add implementation of getComputerChoice

--- rps.js
+++ rps.js
@@ -2,19 +2,27 @@ console.log("Hello World");
 
 function getComputerChoice() {
     // INIT randomVariable with a CALL to getRandomInt(3) to get either 0, 1, 2 randomly
+    const randomVariable = getRandomInt(3);
 
     // IF randomVariable == 0
     // THEN return "rock"
-
-    // IF randomVariable == 1
+    // ELSE IF randomVariable == 1
     // THEN return "paper"
-
-    // IF randomVariable == 2
+    // ELSE
     // THEN return "scissors"
-    
+    if(randomVariable == 0) {
+        return "rock";
+    } else if (randomVariable == 1) {
+        return "paper";
+    } else {
+        return "scissors";
+    }
 }
 
 // Return random number from 0 to max number
 function getRandomInt(max) {
     return Math.floor(Math.random() * max);
 }
+
+console.log("getComputerChoice() result: ");
+console.log(getComputerChoice());
