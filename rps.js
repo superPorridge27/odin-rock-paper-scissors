55c378bf59646639c454bceab5642262cd71643a
Adrianto Puji Irawan <adrianto.irawan@gmail.com>
2026-01-17 08:40:22 +0700

    Add pseudocode of getComputerChoice() and  number randomizer

--- rps.js
+++ rps.js
@@ -1,1 +1,20 @@
 console.log("Hello World");
+
+function getComputerChoice() {
+    // INIT randomVariable with a CALL to getRandomInt(3) to get either 0, 1, 2 randomly
+
+    // IF randomVariable == 0
+    // THEN return "rock"
+
+    // IF randomVariable == 1
+    // THEN return "paper"
+
+    // IF randomVariable == 2
+    // THEN return "scissors"
+    
+}
+
+// Return random number from 0 to max number
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+}
